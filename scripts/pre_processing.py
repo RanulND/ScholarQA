21ff827f21bb601b7eb1d4fdb5f9edd596dae650
UviniR <ranaweeraraua.19@uom.lk>
2024-03-25 15:33:36 +0530

    define preprocessing steps

---
+++ scripts/pre_processing.py
@@ -0,0 +1,12 @@
+# removal of unwanted characters (-) #do this before lowercasing (cus GPT-3 like terms should remian)
+# lowercase 
+                #all the columns lowercasing
+# conversion of abbreviations to long foramts (we need to deinfe the list by our end) - 
+        # make sure the abbrevaitions are converted only if they occur alone
+# remove "x"
+# remove ()
+# remove 1) 
+# remove [4]
+# remove ,
+
+#others should be applied to abstract, keywords, title 
